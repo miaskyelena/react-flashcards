26ffe3f665f9af4d1fcb1c0cf466b96f503a39da
Mia Elena <miaelenatapia@gmail.com>
2023-03-06 15:23:05 -0500

    learn react flashcards

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return(
+    <div>
+      <h1>
+        Test Your Knowledge of the React Javascript Library 
+      </h1>
+      <h3>
+        Developed by 
+        <a href="https://miaelena.netlify.app/">Mia</a>
+      </h3>
+      <h5>Number of Cards: 10</h5>
+    </div>
+  )
+
+}
+
+export default App
