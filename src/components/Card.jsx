bbf4ca20fa4a1d9c84f382bff5e19a161a2bedf1
Mia Elena <miaelenatapia@gmail.com>
2023-03-28 16:56:05 -0400

    ok

--- src/components/Card.jsx
+++
@@ -1,9 +0,0 @@
-const Card = (props) => {    
-    return(
-        <div className={`'Card ' + ${props.categColor}`}>
-            <h1>{props.cardText}</h1>
-            <img className="food-img" src={`${props.img}.png`} />            
-        </div>
-    )
-}
-export default Card
